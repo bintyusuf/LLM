27a10c3d1bc853a9deb9422eb86c1f508468f714
bintyusuf <61346932+bintyusuf@users.noreply.github.com>
2024-03-31 19:33:06 -0700

    Add files via upload

---
+++ sidebar.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+stop_msg = "## STOP AND READ BELOW!!"
+preinfo = """
+You could tweak some values to generate more better articles, let's consider some examples so you could better understand it.
+"""
+example1 = " Dangers of excessive use of social media"
+example2 = """
+Dangers of excessive use of social media --kid\n
+Dangers of excessive use of social media --adult\n
+Dangers of excessive use of social media --athlete\n
+Dangers of excessive use of social media --corporate worker\n
+"""
+example3 = """
+Dangers of excessive use of social media --kid --300\n
+Dangers of excessive use of social media --adult --500\n
+"""
+afterinfo = """
+And you need to use parameters in order you can't use --300 before --kid
+"""
+contactInfo = """
+## For any Queries:-
+### You could connect with me on [LinkedIn](https://www.linkedin.com/in/zynatyusuf/) or could mail me [here](mailto:yusufzinatu@gmail.com)
+"""
+
+examples = [example1, example2, example3]
+
+def sidebar_conf():
+    sidebar = st.sidebar
+    sidebar.title("Zgenerator - A user friendly article generator bot")
+    sidebar.divider()
+    sidebar.markdown(stop_msg)
+    sidebar.caption(preinfo)
+    sidebar.markdown("* To simply generate a article")
+    sidebar.info(examples[0])
+    sidebar.markdown("* To generate articles about certain group like kids, adults, athletes, corporate workers")
+    sidebar.info(examples[1])
+    sidebar.markdown("* If you wanna keep it to certain words")
+    sidebar.info(examples[2])
+    sidebar.caption(afterinfo)
+    sidebar.divider()
+    sidebar.markdown(contactInfo)
+    sidebar.markdown("# Made with ❤️ by Zynat")
+
+if __name__ == "__main__":
+    sidebar_conf()
